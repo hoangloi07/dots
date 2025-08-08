56fe855f09b4e2894228f293455d0511fef52b5e
larry <hoangloi31052007@gmail.com>
2025-08-08 11:00:41 +0700

    unstable changes

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,3 +1,4 @@
 set -g fish_greeting
 export EDITOR="nvim"
 zoxide init fish | source
+set -x LD_LIBRARY_PATH /home/larry/opt/libs $LD_LIBRARY_PATH
---
+++ configs/fish/functions/nvim.fish
@@ -0,0 +1,4 @@
+function nvim --wraps=opt/apps/neovim/bin/nvim --wraps=opt/apps/neovim/share/applications/ --wraps='~/opt/apps/neovim/bin/nvim' --description 'alias nvim=~/opt/apps/neovim/bin/nvim'
+  ~/opt/apps/neovim/bin/nvim $argv
+        
+end
