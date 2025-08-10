4947644d4f2430b878188444f53abcb987688e7e
larry <hoangloi31052007@gmail.com>
2025-08-10 09:58:31 +0700

    tailor for new OS install

--- .config/fish/functions/nvim.fish
+++
@@ -1,4 +0,0 @@
-function nvim --wraps=opt/apps/neovim/bin/nvim --wraps=opt/apps/neovim/share/applications/ --wraps='~/opt/apps/neovim/bin/nvim' --description 'alias nvim=~/opt/apps/neovim/bin/nvim'
-  ~/opt/apps/neovim/bin/nvim $argv
-        
-end
